5eccd5f02983f8f41b1f1aaa140c8b485281d50e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to bind a report to a stored procedure provided by the SQL Data Source in a Silverlight application
+
+
+<p>This example demonstrates how to bind a report to a stored procedure provided by a <a href="https://documentation.devexpress.com/#CoreLibraries/clsDevExpressDataAccessSqlSqlDataSourcetopic">Sql Data Source</a>.  It also shows how to map report parameters to stored procedure parameters. </p>
+<br />In this example, a report is bound to the '<em>CustOrdersOrders'</em> stored procedure (the Northwind data base) with the '<em>CustomerID</em>' parameter.<br /><br />Refer to the <a href="https://documentation.devexpress.com/#XtraReports/CustomDocument10555">How to: Bind a Report to a Stored Procedure</a> documentation for additional details.<br /><br />See also:<br /><a href="https://documentation.devexpress.com/#XtraReports/CustomDocument9997">Parameters Overview</a>
+
+<br/>
+
+
---
+++ VB/DXSilverlightApplication1/My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("DXSilverlightApplication1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DXSilverlightApplication1")>
+<Assembly: AssemblyCopyright("Copyright ©  2015")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("77ea377f-5cf1-48c4-8996-d41c4eeb8f19")>
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Revision and Build Numbers 
+' by using the '*' as shown below:
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
